bce6a8fb89ee0156acfbce02eded1eab2792ebb4
gusta <gustavo024dias@gmail.com>
2024-01-09 14:37:55 -0300

    correcao de nan

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import base64
 from pymongo import MongoClient
 import os
+
 # > pyinstaller  spec.spec
 client = MongoClient(
     'mongodb+srv://henriquedb:Y2du8cnwhZYzjrlE@cluster0.vgf811c.mongodb.net/?retryWrites=true&w=majority')
@@ -21,14 +22,29 @@ def atualizar_banco(tabela_excel):
     lista_dados = []
     for indice, data in df.iterrows():
         sku = str(data.iloc[2])
+        estoque = data.iloc[9]
+        nome = data.iloc[3]
+        preco = data.iloc[12]
+
+        if str(estoque).lower() == "nan":
+            estoque = 0.0
+
+        if str(nome).lower() == "nan":
+            nome = ""
+
+        if str(preco).lower() == "nan":
+            preco = 0.0
+
         print(f"Produto {indice} registrado!")
+
         lista_dados.append({
             "_id": base64.b64encode(sku.encode('utf-8')).decode('utf-8'),
             "sku": sku,
-            "estoque": data.iloc[9],
-            "nome": data.iloc[3],
-            "preco": data.iloc[12]
+            "estoque": estoque,
+            "nome": nome,
+            "preco": preco
         })
+
     print("Inserindo no banco de dados...")
     col.insert_many(lista_dados)
     print("Produtos atualizados no banco de dados!")
@@ -45,10 +61,10 @@ for arquivo in arquivos:
         if ex_arquivo[1] in ALLOWED_EXTENSIONS:
             lista_arq.append(arquivo)
 
+print("Versão 2.0 - Correção de valores nulos")
 input(f"Foram reconhecidos {len(lista_arq)} arquivos de excel, escolha qual deve ser usado: ok!")
 for arquivo in lista_arq:
     resposta = int(input(f"{arquivo} SIM (1) ou não (2): "))
     if resposta == 1:
         print("Atualizando, aguarde...")
         atualizar_banco(arquivo)
-
