984b7d1e28637d8379ee73fe07e3654f314fb61f
gusta <gustavo024dias@gmail.com>
2024-03-08 19:44:12 -0300

    versao 5

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import base64
 from pymongo import MongoClient
 import os
 import uuid
+import requests
+from datetime import datetime
 
 # > pyinstaller  spec.spec
 # winclassbenicio@gmail.com
@@ -13,8 +15,40 @@ db = client['db_produtos']
 col = db['col_produtos']
 
 col_clientes = db['col_clientes']
+col_distribuidores = db['col_distribuidores']
 
 ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
+
+
+def atualizar_distribuidores(tabela_excel):
+    linha_inicial = 1
+
+    df = pd.read_excel(tabela_excel, skiprows=range(1, linha_inicial))
+
+    lista_dados = []
+    for indice, data in df.iterrows():
+        empresa = str(data.iloc[0])
+        valorMinimo = str(data.iloc[4])
+        frete = str(data.iloc[5])
+        meioPagamento = str(data.iloc[6])
+        prazo = str(data.iloc[7])
+        aceitaVouche = str(data.iloc[10])
+
+        lista_dados.append({
+            "empresa": empresa,
+            "valorMinimo": valorMinimo,
+            "frete": frete,
+            "meioPagamento": meioPagamento,
+            "prazo": prazo,
+            "aceitaVouche": aceitaVouche
+        })
+
+        print(f"Distribuidor {indice} registrado!")
+
+    print("Inserindo no banco de dados...")
+    col_distribuidores.insert_many(lista_dados)
+    print("Produtos atualizados no banco de dados!")
+    input("ok!")
 
 
 def atualizar_produtos(tabela_excel):
@@ -47,7 +81,7 @@ def atualizar_produtos(tabela_excel):
         print(f"Produto {indice} registrado!")
 
         lista_dados.append({
-            "_id": base64.b64encode(sku.encode('utf-8')).decode('utf-8'),
+            "_id": base64.b64encode(str(uuid.uuid4()).encode('utf-8')).decode('utf-8'),
             "sku": sku,
             "estoque": estoque,
             "nome": nome,
@@ -58,6 +92,19 @@ def atualizar_produtos(tabela_excel):
     print("Inserindo no banco de dados...")
     col.insert_many(lista_dados)
     print("Produtos atualizados no banco de dados!")
+    data_hora_atual = datetime.now()
+
+    formato = "%Y-%m-%d %H:%M:%S"
+    data_hora_formatada = data_hora_atual.strftime(formato)
+
+    url_put = "https://redcloudapppedidos-default-rtdb.firebaseio.com/info.json"
+
+    dados = {"dataHora": data_hora_formatada}
+
+    response = requests.put(url_put, json=dados)
+
+    if response.status_code == 200:
+        print(data_hora_formatada)
     input("ok!")
 
 
@@ -147,8 +194,10 @@ for arquivo in arquivos:
         if ex_arquivo[1] in ALLOWED_EXTENSIONS:
             lista_arq.append(arquivo)
 
-print("Versão 4.0 - Updates -> https://pastebin.com/Ef9CUMcW ")
-clienteOrProduto = input("Atualizar Clientes ( 1 )  -  Atualizar Produtos ( 2 ou ENTER ): ")
+print("Versão 5.0")
+
+clienteOrProduto = input("Atualizar Clientes ( 1 )\nAtualizar Produtos ( 2 )\nAtualizar Distribuidores ( 3 )\nR - ")
+
 input(f"Foram reconhecidos {len(lista_arq)} arquivos de excel, escolha qual deve ser usado: ok!")
 for arquivo in lista_arq:
     resposta = input(f"{arquivo} NÃO (1) ou SIM (2 ou ENTER): ")
@@ -157,6 +206,8 @@ for arquivo in lista_arq:
 
         if clienteOrProduto == "1":
             atualizar_cliente(arquivo)
-        else:
+        elif clienteOrProduto == "2":
             atualizar_produtos(arquivo)
+        elif clienteOrProduto == "3":
+            atualizar_distribuidores(arquivo)
         break
